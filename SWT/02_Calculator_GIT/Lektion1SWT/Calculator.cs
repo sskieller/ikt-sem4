6bcf782f5885191b39b5c8a536825e60ec124a3e
JAgger <au524731@uni.au.dk>
2018-02-01 10:41:18 +0100

    added multiply

---
+++ SWT/02_Calculator_GIT/Lektion1SWT/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lektion1SWT
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Do nothing
+        }
+    }
+    public class CalculatorUnit
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
