39430f689be614586e8fa011dd1f89ebf3c654b6
JAgger <Jonasagger@hotmail.com>
2018-02-22 10:41:09 +0100

    added small things to the client+server

--- IKN/01_TCPServer/TCPServer/file_client.py
+++ IKN/01_TCPServer/TCPServer/file_client.py
@@ -1,15 +1,27 @@
 import sys
-from socket import *
+import socket
 from lib import Lib
 
 PORT = 9000
 BUFSIZE = 1000
 
+
 def main(argv):
-	# TO DO Your Code
-    
-def receiveFile(fileName,  conn):
-	# TO DO Your Code
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    clientsocket.connect(("10.0.0.1", PORT))
+
+    clientsocket.send("hello world".encode('ascii'))
+
+    msg = clientsocket.recv(1000)
+
+    clientsocket.close()
+
+    print(msg.decode('ascii'))
+
+def receiveFile(fileName, conn):
+    pass
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
--- IKN/01_TCPServer/TCPServer/file_server.py
+++ IKN/01_TCPServer/TCPServer/file_server.py
@@ -7,10 +7,29 @@ PORT = 9000
 BUFSIZE = 1000
 
 def main(argv):
-	# TO DO Your Code
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind(("10.0.0.1", 9000))
+
+    serversocket.listen(5)
+
+
+    while True:
+        clientsocket, addr = serversocket.accept()
+
+        print("got a connection from %s", addr)
+
+        msg = clientsocket.recv(1000)
+
+        print("message received: %s", msg.decode('ascii'))
+
+        msg = "Hello client!"
+        clientsocket.send(msg.encode('ascii'))
+        clientsocket.close()
+
 
 def sendFile(fileName,  fileSize,  conn):
-	# TO DO Your Code
+    pass
+    # TO DO Your Code
     
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
