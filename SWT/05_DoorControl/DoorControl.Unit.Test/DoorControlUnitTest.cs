6d588a755739fe41f06c2b89c6b06f4f2e2f5d0f
Simon Vu <simon_vu1@hotmail.com>
2018-02-22 12:16:16 +0100

    Added DoorControl to Git

---
+++ SWT/05_DoorControl/DoorControl.Unit.Test/DoorControlUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using NSubstitute.Core;
+
+namespace DoorControl.Unit.Test
+{
+	[TestFixture]
+    public class DoorControlUnitTest
+	{
+		private DoorControl _doorControl;
+		private IDoor _fakeDoor;
+		private IEntryNotification _fakeNotification;
+		private IUserValidation _fakeUserValidation;
+		
+		[SetUp]
+	    public void Setup()
+	    {
+		    _fakeDoor = Substitute.For<IDoor>();
+		    _fakeNotification = Substitute.For<IEntryNotification>();
+		    _fakeUserValidation = Substitute.For<IUserValidation>();
+		    _doorControl = new DoorControl(_fakeDoor, _fakeUserValidation, _fakeNotification);
+	    }
+
+	    [Test]
+	    public void RequestEntry_ValidId_ExpectAccessGranted()
+	    {
+		    _fakeUserValidation.ValidateEntryRequest(Arg.Any<string>()).Returns(true);
+			_doorControl.RequestEntry(Arg.Any<string>());
+			_fakeNotification.Received().NotifyEntryGranted();
+	    }
+
+		[Test]
+		public void RequestEntry_InvalidId_ExpectAccessDenied()
+		{
+			_fakeUserValidation.ValidateEntryRequest(Arg.Any<string>()).Returns(false);
+			_doorControl.RequestEntry(Arg.Any<string>());
+			_fakeNotification.Received().NofifyEntryDenied();
+		}
+    }
+}
---
+++ SWT/05_DoorControl/DoorControl/DoorControl.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControl
+{
+	public class DoorEventArgs : EventArgs
+	{
+		public bool Forced { get; set; }
+	}
+
+	public class DoorControl
+    {
+	    public event EventHandler DoorOpen;
+	    public event EventHandler DoorClosed;
+
+	    private IDoor _door;
+	    private IUserValidation _userValidation;
+	    private IEntryNotification _entryNotification;
+		public DoorControl(IDoor door, IUserValidation userValidation, IEntryNotification entryNotification)
+		{
+			_door = door;
+			_userValidation = userValidation;
+			_entryNotification = entryNotification;
+		}
+
+	    public void RequestEntry(string id)
+	    {
+		    if (_userValidation.ValidateEntryRequest(id) == true)
+		    {
+			    _door.Open();
+				_entryNotification.NotifyEntryGranted();
+		    }
+		    else
+		    {
+			    _entryNotification.NofifyEntryDenied();
+		    }
+	    }
+
+	    protected virtual void OnDoorOpen(DoorEventArgs e)
+	    {
+		    EventHandler handler = DoorOpen;
+		    if (DoorOpen != null)
+		    {
+				DoorOpenHandler(this, e);
+		    }
+	    }
+
+	    private void DoorOpenHandler(object sender, DoorEventArgs e)
+	    {
+		    if (e.Forced == true)
+		    {
+				_entryNotification.SignalAlarm();
+		    }
+		    else
+		    {
+				
+		    }
+	    }
+    }
+}
