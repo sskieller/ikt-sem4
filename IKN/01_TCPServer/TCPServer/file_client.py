a99a24960b4f5fe16c50507d0e1b4af2d6d20772
JAgger <Jonasagger@hotmail.com>
2018-02-22 09:32:48 +0100

    added files

---
+++ IKN/01_TCPServer/TCPServer/file_client.py
@@ -0,0 +1,15 @@
+import sys
+from socket import *
+from lib import Lib
+
+PORT = 9000
+BUFSIZE = 1000
+
+def main(argv):
+	# TO DO Your Code
+    
+def receiveFile(fileName,  conn):
+	# TO DO Your Code
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ IKN/01_TCPServer/TCPServer/file_server.py
@@ -0,0 +1,16 @@
+import sys
+import socket
+from lib import Lib
+
+HOST = ''
+PORT = 9000
+BUFSIZE = 1000
+
+def main(argv):
+	# TO DO Your Code
+
+def sendFile(fileName,  fileSize,  conn):
+	# TO DO Your Code
+    
+if __name__ == "__main__":
+   main(sys.argv[1:])
