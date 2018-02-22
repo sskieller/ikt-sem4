bd6b0683330e96f1e6e4c208a5e31f42fcda9218
JAgger <Jonasagger@hotmail.com>
2018-02-22 14:52:30 +0100

    Added Comments ples

--- IKN/01_TCPServer/TCPServer/file_server.py
+++ IKN/01_TCPServer/TCPServer/file_server.py
@@ -2,42 +2,48 @@ import sys
 import socket
 from TCPLib import *
 
-HOST = ''
+HOST = "10.0.0.1"
 PORT = 9000
 BUFSIZE = 1000
 
+
 def main(argv):
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(("10.0.0.1", 9000))
+    serversocket.bind((HOST, PORT))
 
     serversocket.listen(5)
 
 
     while True:
+        # Accepting Connection
         clientsocket, addr = serversocket.accept()
 
-        print("got a connection from %s", addr)
+        print("got a connection from {}".format(addr))
 
+        # Getting file request
         msg = readTextTCP(clientsocket)
 
-        print("message received: ", msg)
+        print("message received: {}".format(msg))
 
+        # If close, close server, used for debugging and quick resetting the server
         if msg == "close":
             serversocket.close()
             sys.exit()
+
+        # Getting filesize of the file, will return 0, if file not found
         size = check_File_Exists(extractFilename(msg))
 
         if size > 0:
-            returnmsg = str(size)
-            writeTextTCP(returnmsg, clientsocket)
+            returnmsg = str(size)  # Making the size a string to be sent
+            writeTextTCP(returnmsg, clientsocket)  # Sending filesize as a string
 
-            file = open(msg, 'rb')
+            file = open(msg, 'rb') # Opens file
 
-            filedata = file.read(BUFSIZE)
+            filedata = file.read(BUFSIZE)  # Reads BUFSIZE bytes in from the file
 
             while filedata:
-                clientsocket.send(filedata)
-                filedata = file.read(BUFSIZE)
+                clientsocket.send(filedata)  # Send filedata
+                filedata = file.read(BUFSIZE)  # Read more bytes.
 
 
         #writeTextTCP(msg, clientsocket)
