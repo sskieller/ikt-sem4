e65496781cb4b5bdde78f872924b52e8a278f887
sskieller <soren.skieller@gmail.com>
2018-03-13 13:04:59 +0100

    added existing model for db

---
+++ PRJ4/WebApi/FWPS/Models/FWPS_DBContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FWPS.Models
+{
+    public partial class FWPS_DBContext : DbContext
+    {
+        public virtual DbSet<LightObject> LightObject { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LightObject>(entity =>
+            {
+                entity.HasKey(e => e.LightId);
+
+                entity.Property(e => e.Command)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LastModifiedDate).HasColumnType("datetime");
+            });
+        }
+
+        public FWPS_DBContext(DbContextOptions<LightContext> options)
+            : base(options)
+        { }
+    }
+}
---
+++ PRJ4/WebApi/FWPS/Models/LightObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FWPS.Models
+{
+    public partial class LightObject
+    {
+        public int LightId { get; set; }
+        public string Command { get; set; }
+        public bool IsRun { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
