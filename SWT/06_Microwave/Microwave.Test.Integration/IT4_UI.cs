51b83a99e280eb6fa1c9917b0afc3d7be5220526
Simon Vu <simon_vu1@hotmail.com>
2018-03-16 13:02:10 +0100

    Added last tests (empty) as well as application as base

---
+++ SWT/06_Microwave/Microwave.Test.Integration/IT4_UI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using MicrowaveOvenClasses;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+
+namespace Microwave.Test.Integration
+{
+	[TestFixture]
+	class IT4_UI
+	{
+		private UserInterface _ui;
+		private IDisplay _display;
+		private ILight _light;
+		private IButton _button;
+	}
+}
