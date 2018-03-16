fc0b4274bd3fe46344c5576d93d7b6f0740c685a
sskieller <soren.skieller@gmail.com>
2018-03-16 14:16:17 +0100

    added PowerTube testing

--- SWT/06_Microwave/Microwave.Test.Integration/IT1_PowerTube.cs
+++ SWT/06_Microwave/Microwave.Test.Integration/IT1_PowerTube.cs
@@ -27,13 +27,43 @@ namespace Microwave.Test.Integration
             _uut = new PowerTube(_output);
         }
 
-        [TestCase(1)]
-        [TestCase(100, "PowerTube works with 100 %", TestName = "Powerlevel 100")]
-        public void TurnOn_PowerLevelOK_Allowed(int number, string expectedOutput)
+        [TestCase(50, "PowerTube works with 50 W\r\n", TestName = "Powerlevel 50W")]
+        [TestCase(700, "PowerTube works with 700 W\r\n", TestName = "Powerlevel 700W")]
+        public void TurnOn_PowerLevelOK_Allowed(int power, string expectedOutput)
         {
-            _uut.TurnOn(number);
+            _uut.TurnOn(power);
 
             Assert.That(_sw.ToString(), Is.EqualTo(expectedOutput));
+        }
+
+        [TestCase(49, TestName = "Powerlevel 49W")]
+        [TestCase(701, TestName = "Powerlevel 701W")]
+        public void TurnOn_PowerLevelOutOfRange_NotAllowed(int power)
+        {
+            Assert.That(() => _uut.TurnOn(power), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [TestCase(100, "PowerTube works with 100 W\r\n", TestName = "Powerlevel 100W NotOn")]
+        public void TurnOn_PowerlevelOK_IsNotOn(int power, string expectedOutput)
+        {
+            _uut.TurnOn(power);
+            Assert.That(() => _sw.ToString(), Is.EqualTo(expectedOutput));
+        }
+
+        [TestCase(100, TestName = "AlreadyOn, Powerlevel 100")]
+        public void TurnOn_IsAlreadyOn_NotAllowed(int power)
+        {
+            _uut.TurnOn(power);
+            Assert.That(() => _uut.TurnOn(power), Throws.TypeOf<ApplicationException>());
+        }
+
+        [Test]
+        public void TurnOff_IsAlreadyOn_Allowed()
+        {
+            _uut.TurnOn(100);
+            _sw.GetStringBuilder().Clear();
+            _uut.TurnOff();
+            Assert.That(() => _sw.ToString(), Is.EqualTo("PowerTube turned off\r\n"));
         }
     }
 }
