16cbfde88a64a13dcc6a739cb95fff99e5ec992a
Simon Vu <simon_vu1@hotmail.com>
2018-03-16 12:09:41 +0100

    Added integration project to repo

---
+++ SWT/06_Microwave/Microwave.Test.Integration/LightIntegrationTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+	[TestFixture]
+    public class LightIntegrationTest
+    {
+    }
+}
