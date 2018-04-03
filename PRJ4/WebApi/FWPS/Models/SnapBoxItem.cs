1bc8f5d74b98452a169c92842a9b94148d641e26
Simon Vu <Simon_vu1@hotmail.com>
2018-04-03 21:53:33 +0200

    Changed model for SnpaBoxItem to better represent the one actually used

--- PRJ4/WebApi/FWPS/Models/SnapBoxItem.cs
+++ PRJ4/WebApi/FWPS/Models/SnapBoxItem.cs
@@ -8,10 +8,11 @@ namespace FWPS.Models
 {
     public class SnapBoxItem : ItemBase
     {
-
+        public string SnapBoxId { get; set; }
         public string PowerLevel { get; set; }
         public bool MailReceived { get; set; }
         public string ReceiverEmail { get; set; }
+        public string Checksum { get; set; }
 
     }
 }
