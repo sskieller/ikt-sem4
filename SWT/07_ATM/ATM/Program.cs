dd6dc39e3fdf62689a2b07d00ad0f4015a919bcf
JAgger <Jonasagger@hotmail.com>
2018-04-05 12:03:02 +0200

    added plane

--- SWT/07_ATM/ATM/Program.cs
+++ SWT/07_ATM/ATM/Program.cs
@@ -13,22 +13,6 @@ namespace ATM
         {
             //var atm = new ATM();
 
-            TransponderDataParser dp = new TransponderDataParser();
-
-            DateTime dt;
-            string tag;
-            int x;
-            int y;
-            uint alt;
-
-            dp.ParseData("ATR423;40000;10000;14000;20180405113000123", out tag, out x, out y, out alt, out dt);
-
-            Console.WriteLine(tag);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(alt);
-            Console.WriteLine(dt);
-
             //while (Console.Read() != 'q') ;
         }
     }
@@ -36,17 +20,23 @@ namespace ATM
     public class ATM
     {
         private readonly ITransponderReceiver _receiver;
+        private ITransponderDataParser _dataParser;
+
+        private List<Plane> _planes = new List<Plane>();
+
         public ATM()
         {
+            _dataParser = new TransponderDataParser();
             _receiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
             _receiver.TransponderDataReady += ReceiverOnTransponderDataReady;
         }
 
         private void ReceiverOnTransponderDataReady(object sender, RawTransponderDataEventArgs rawTransponderDataEventArgs)
         {
-            foreach(var data in rawTransponderDataEventArgs.TransponderData)
-                Console.WriteLine(data);
-            //Console.WriteLine(rawTransponderDataEventArgs.TransponderData);
+            foreach (var data in rawTransponderDataEventArgs.TransponderData)
+            {
+
+            }
         }
     }
 
