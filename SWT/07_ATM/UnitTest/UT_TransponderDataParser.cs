603bc83a0a681c543582fa72b492cd008d5ab286
sskieller <soren.skieller@gmail.com>
2018-04-05 14:21:21 +0200

    Added working version with test

---
+++ SWT/07_ATM/UnitTest/UT_TransponderDataParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ATM;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class UT_TransponderDataParser
+    {
+
+        private ITransponderDataParser uut;
+    }
+}
