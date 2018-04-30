093f16d9ba839619e7566d17a69851aecd1d7436
sskieller <soren.skieller@gmail.com>
2018-04-30 12:39:14 +0200

    added several UTs for lightController

--- PRJ4/WebApi/WebApi.Unit.Test/UT_LightController.cs
+++ PRJ4/WebApi/WebApi.Unit.Test/UT_LightController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Web.Http;
 using FWPS;
 using FWPS.Controllers;
 using FWPS.Data;
@@ -11,9 +13,12 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Rest.TransientFaultHandling;
+using System.Web.Http.Results;
 using NSubstitute;
 using NUnit.Framework;
-
+using BadRequestResult = Microsoft.AspNetCore.Mvc.BadRequestResult;
+using NotFoundResult = Microsoft.AspNetCore.Mvc.NotFoundResult;
 
 
 namespace WebApi.Unit.Test
@@ -70,6 +75,122 @@ namespace WebApi.Unit.Test
         }
 
         [Test]
+        public void Newest_NoItems_ExpectedResult_NotFound()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+
+                _lc.Delete(1);
+
+                IActionResult result = _lc.Next();
+
+                Assert.IsInstanceOf<NotFoundResult>(result);
+            }
+        }
+
+        [Test]
+        public void Newest_ThreeLightItems_ExpectedResult_LastItemOnly()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+                _lc.Create(_li);
+                _lc.Create(_li2);
+
+                var result = _lc.Newest() as ObjectResult;
+                var model = result.Value as LightItem;
+
+                Assert.That(model.Command, Is.EqualTo("What"));
+                Assert.That(model.Command, Is.EqualTo("What"));
+            }
+        }
+
+        [Test]
+        public void Next_NoItems_ExpectedResult_NotFound()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+
+                _lc.Delete(1);
+
+                IActionResult result = _lc.Next();
+
+                Assert.IsInstanceOf<NotFoundResult>(result);
+            }
+        }
+
+        [Test]
+        public void Next_ThreeLightItems_ExpectedResult_FirstItemOnly()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+                _lc.Create(_li);
+                _lc.Create(_li2);
+
+                var result = _lc.Next() as ObjectResult;
+                var model = result.Value as LightItem;
+
+                Assert.That(model.Command, Is.EqualTo("on"));
+                Assert.That(model.Command, Is.EqualTo("on"));
+            }
+        }
+
+        [Test]
+        public void GetAll_ThreeLightItems_ExpectedResult_Command_on_What_What()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+                _lc.Create(_li);
+                _lc.Create(_li2);
+
+                var results = _lc.GetAll().ToList();
+
+                Assert.That(results[0].Command, Is.EqualTo("on"));
+                Assert.That(results[1].Command, Is.EqualTo("What"));
+                Assert.That(results[2].Command, Is.EqualTo("What"));
+            }
+        }
+
+        [Test]
+        public void GetAll_ThreeLightItems_ExpectedResult_3()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context,_hub);
+                _lc.Create(_li);
+                _lc.Create(_li2);
+
+                var results = _lc.GetAll().ToList();
+
+                Assert.That(results.Count, Is.EqualTo(3));
+            }
+        }
+
+        [Test]
+        public void GetAll_ListOfItems_ExpectedResult_ReturnedView()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+                _lc.Create(_li);
+                _lc.Create(_li2);
+
+                Assert.That(_lc.GetAll(), Is.EqualTo(_context.LightItems.ToList()));
+            }
+        }
+
+        [Test]
         public void GetById_IdNotExisting_ExpectedResult_NullReferenceException()
         {
             using (_context = new FwpsDbContext(_options))
@@ -103,6 +224,22 @@ namespace WebApi.Unit.Test
                 var model = result?.Value as LightItem;
 
                 Debug.Assert(model != null, nameof(model) + " != null");
+                Assert.That(model.Id, Is.EqualTo(id));
+            }
+        }
+
+        [Test]
+        public void Constructor_CorrectContext_Expectedresult_LightItemAdded()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+                var id = 1;
+
+                var result = _lc.GetById(id) as ObjectResult;
+                var model = result?.Value as LightItem;
+
                 Assert.That(model.Id, Is.EqualTo(id));
             }
         }
@@ -298,7 +435,83 @@ namespace WebApi.Unit.Test
         }
 
         [Test]
-        public void GetAll_ListOfItems_ExpectedResult_ReturnedView()
+        public void Update_LightItemNull_ExpectedResult_BadRequest()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+
+                LightItem _li3 = null;
+
+                IActionResult result = _lc.Update(4, _li3);
+
+                Assert.IsInstanceOf<BadRequestResult>(result);
+            }
+        }
+
+        [Test]
+        public void Update_LightItemWrongId_ExpectedResult_BadRequest()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+
+                IActionResult result = _lc.Update(5, _li2);
+
+                Assert.IsInstanceOf<BadRequestResult>(result);
+            }
+        }
+
+        [Test]
+        public void Update_NoItemsInDatabase_ExpectedResult_NotFound()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+
+                _lc.Delete(1);
+
+                IActionResult result = _lc.Update(0, _li);
+
+                Assert.IsInstanceOf<NotFoundResult>(result);
+            }
+        }
+
+        [Test]
+        public void Delete_OneItemInDatabase_ExpectedResult_NoItemsLeft()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+
+                IActionResult result = _lc.Delete(1);
+
+                Assert.That(_lc.GetAll().ToList().Count, Is.EqualTo(0));
+            }
+        }
+
+        [Test]
+        public void Delete_ItemAlreadyDeleted_ExpectedResult_NotFound()
+        {
+            using (_context = new FwpsDbContext(_options))
+            {
+                _context.Database.EnsureCreated();
+                _lc = new LightController(_context, _hub);
+
+                _lc.Delete(1);
+
+                IActionResult result = _lc.Delete(1);
+
+                Assert.IsInstanceOf<NotFoundResult>(result);
+            }
+        }
+
+        [Test]
+        public void Delete_SeveralDeletionsFromDatabase_ExpectedResult_NoItemsLeft()
         {
             using (_context = new FwpsDbContext(_options))
             {
@@ -307,9 +520,28 @@ namespace WebApi.Unit.Test
                 _lc.Create(_li);
                 _lc.Create(_li2);
 
-                Assert.That(_lc.GetAll(), Is.EqualTo(_context.LightItems.ToList()));
+                _lc.Delete(1);
+                _lc.Delete(2);
+                _lc.Delete(3);
+
+                Assert.That(_lc.GetAll().ToList().Count, Is.EqualTo(0));
             }
         }
+
+        //{
+        //    using (_context = new FwpsDbContext(_options))
+        //    {
+        //        _context.Database.EnsureCreated();
+        //        _lc = new LightController(_context, _hub);
+
+        //        _lc.Delete(1);
+
+        //        IActionResult result = _lc.Next();
+
+        //        Assert.IsInstanceOf<NotFoundResult>(result);
+        //    }
+        //}
+
 
         //_hub.Clients.All.Received(1).InvokeAsync(Arg.Is<string>( bob => bob == "UpdateSpecific"), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<object>());
 
