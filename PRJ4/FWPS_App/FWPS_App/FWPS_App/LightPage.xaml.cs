42d23d525d8a3b601205ff4ac756989a299ec52a
Magnus Dewett <Magnusdew@gmail.com>
2018-05-01 16:51:38 +0200

    Light should be completely done. WakeUp and Sleep logic implemented and error handling works.

--- PRJ4/FWPS_App/FWPS_App/FWPS_App/LightPage.xaml.cs
+++ PRJ4/FWPS_App/FWPS_App/FWPS_App/LightPage.xaml.cs
@@ -12,23 +12,21 @@ using System.Threading;
 
 namespace FWPS_App
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class LightPage : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LightPage : ContentPage
+    {
         System.Timers.Timer timer;
-        public LightPage ()
-		{
-			InitializeComponent ();
+        public LightPage()
+        {
+            InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
             LightState();
+            Timer();
+            GetWakeUpAndSleepTimes();
+            ReturnBtn.Clicked += ReturnBtn_Clicked;
             OnButton.Clicked += OnButton_Clicked;
             OffButton.Clicked += OffButton_Clicked;
-            ReturnBtn.Clicked += ReturnBtn_Clicked;
-            timer = new System.Timers.Timer();
-            timer.Elapsed += (object s, ElapsedEventArgs e) => LightState();
-            timer.AutoReset = true;
-            timer.Interval = 2000;
-            timer.Start();
+            WakeUpAndSleepApplyBtn.Clicked += WakeUpAndSleepApplyBtn_Clicked;
         }
 
         private void ReturnBtn_Clicked(object sender, EventArgs e)
@@ -36,13 +34,139 @@ namespace FWPS_App
             Navigation.PopAsync();
         }
 
-        private void LightState()
+        private void GetWakeUpAndSleepTimes()
         {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                string obj = HTTPRequestHandler.CreateGetRequest(LightUri + "newest/").ToString();
+
+                var lightObject = JsonConvert.DeserializeObject<LightObject>(obj);
+
+                hhWakeUpEditor.Text = lightObject.WakeUpTime.Hour.ToString();
+                mmWakeUpEditor.Text = lightObject.WakeUpTime.Minute.ToString();
+                hhSleepEditor.Text = lightObject.SleepTime.Hour.ToString();
+                mmSleepEditor.Text = lightObject.SleepTime.Minute.ToString();
+            });
+        }
+
+        private void WakeUpAndSleepApplyBtn_Clicked(object sender, EventArgs e)
+        {
+            WakeUpAndSleepApplyBtn.IsEnabled = false;
+            // The four editor fiels has to hold integer values
+            if (!int.TryParse((hhWakeUpEditor.Text), out int something))
+            {
+                DisplayAlert("Wrong input", "Field(s) either empty or wrong input type. Please try again", "OK");
+                hhWakeUpEditor.Text = "";
+                WakeUpAndSleepApplyBtn.IsEnabled = true;
+                return;
+            }
+
+            if (!int.TryParse((mmWakeUpEditor.Text), out int something2))
+            {
+                DisplayAlert("Wrong input", "Field(s) either empty or wrong input type. Please try again", "OK");
+                mmWakeUpEditor.Text = "";
+                WakeUpAndSleepApplyBtn.IsEnabled = true;
+                return;
+            }
+
+            if (!int.TryParse((hhSleepEditor.Text), out int something3))
+            {
+                DisplayAlert("Wrong input", "Field(s) either empty or wrong input type. Please try again", "OK");
+                hhSleepEditor.Text = "";
+                WakeUpAndSleepApplyBtn.IsEnabled = true;
+                return;
+            }
+
+            if (!int.TryParse((mmSleepEditor.Text), out int something4))
+            {
+                DisplayAlert("Wrong input", "Field(s) either empty or wrong input type. Please try again", "OK");
+                mmSleepEditor.Text = "";
+                WakeUpAndSleepApplyBtn.IsEnabled = true;
+                return;
+            }
+
+
+            var hhWakeUpInput = Double.Parse(hhWakeUpEditor.Text);
+            var mmWakeUpInput = Double.Parse(mmWakeUpEditor.Text);
+            var hhSleepInput = Double.Parse(hhSleepEditor.Text);
+            var mmSleepInput = Double.Parse(mmSleepEditor.Text);
+
+            // Check if inputs are within ranges
+            if (hhWakeUpInput < 0 || hhWakeUpInput > 23)
+            {
+                DisplayAlert("Wrong input", "Input(s) out of range. Please try again", "OK");
+                hhWakeUpEditor.Text = "";
+                WakeUpAndSleepApplyBtn.IsEnabled = true;
+                return;
+            }
+
+            if (mmWakeUpInput < 0 || mmWakeUpInput > 59)
+            {
+                DisplayAlert("Wrong input", "Input(s) out of range. Please try again", "OK");
+                mmWakeUpEditor.Text = "";
+                WakeUpAndSleepApplyBtn.IsEnabled = true;
+                return;
+            }
+
+            if (hhSleepInput < 0 || hhSleepInput > 23)
+            {
+                DisplayAlert("Wrong input", "Input(s) out of range. Please try again", "OK");
+                hhSleepEditor.Text = "";
+                WakeUpAndSleepApplyBtn.IsEnabled = true;
+                return;
+            }
+
+            if (mmSleepInput < 0 || mmSleepInput > 59)
+            {
+                DisplayAlert("Wrong input", "Input(s) out of range. Please try again", "OK");
+                mmSleepEditor.Text = "";
+                WakeUpAndSleepApplyBtn.IsEnabled = true;
+                return;
+            }
+
+
+            DateTime WakeUpParsedDate = new DateTime();
+            WakeUpParsedDate = DateTime.Today;
+            WakeUpParsedDate = WakeUpParsedDate.AddHours(hhWakeUpInput);
+            WakeUpParsedDate = WakeUpParsedDate.AddMinutes(mmWakeUpInput);
+
+            DateTime SleepParsedDate = new DateTime();
+            SleepParsedDate = DateTime.Today;
+            SleepParsedDate = SleepParsedDate.AddHours(hhSleepInput);
+            SleepParsedDate = SleepParsedDate.AddMinutes(mmSleepInput);
+
 
             string obj = HTTPRequestHandler.CreateGetRequest(LightUri + "newest/").ToString();
 
             var lightObject = JsonConvert.DeserializeObject<LightObject>(obj);
 
+            LightObject lightWakeUpObject = new LightObject()
+            {
+                IsOn = lightObject.IsOn,
+                Command = "UpdateTime",
+                WakeUpTime = WakeUpParsedDate,
+                SleepTime = SleepParsedDate
+            };
+            HTTPRequestHandler.CreateRequest(lightWakeUpObject, LightUri);
+            WakeUpAndSleepApplyBtn.IsEnabled = true;
+        }
+
+
+        private void Timer()
+        {
+            timer = new System.Timers.Timer();
+            timer.Elapsed += (object s, ElapsedEventArgs e) => LightState();
+            timer.AutoReset = true;
+            timer.Interval = 2000;
+            timer.Start();
+        }
+
+    private void LightState()
+        {
+
+            string obj = HTTPRequestHandler.CreateGetRequest(LightUri + "newest/").ToString();
+
+            var lightObject = JsonConvert.DeserializeObject<LightObject>(obj);
 
             if (lightObject.IsOn == true)
             {
@@ -75,17 +199,17 @@ namespace FWPS_App
         private void OffButton_Clicked(object sender, EventArgs e)
         {
 
-                OnButton.IsEnabled = false;
+            OffButton.IsEnabled = false;
 
-                LightObject lightObject = new LightObject()
-                {
-                    Command = "off",
-                    IsRun = false,
-                    IsOn = false
-                };
+            LightObject lightObject = new LightObject()
+            {
+                Command = "off",
+                IsRun = false,
+                IsOn = false
+            };
             HTTPRequestHandler.CreateRequest(lightObject, LightUri);
-            OnButton.IsEnabled = true;
-            
+            OffButton.IsEnabled = true;
+
         }
 
         private void OnButton_Clicked(object sender, EventArgs e)
@@ -113,4 +237,4 @@ namespace FWPS_App
             public DateTime WakeUpTime { get; set; }
         }
     }
-}   
+}
