cfe3e9bf9f58a10e45cb6072904d0439915a7581
Simon Vu <Simon_vu1@hotmail.com>
2018-05-03 13:57:24 +0200

    hello

---
+++ PRJ4/MasterApplication/MasterApplication.Unit.Test/UT_FwpsListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MasterApplication.Threads;
+using NSubstitute;
+using NUnit.Framework;
+using RabbitMQ.Client;
+
+namespace MasterApplication.Unit.Test
+{
+    [TestFixture]
+    class UT_FwpsListener
+    {
+        private FwpsListener _listener;
+        private IConnection _connection;
+        [SetUp]
+        public void SetUp()
+        {
+            
+            _connection = Substitute.For<IConnection>();
+            _listener = new FwpsListener(_connection);
+        }
+
+        [Test]
+        public void Add_AddTopic_TopicAdded()
+        {
+            _listener.Add("Test");
+            Assert.That(true);
+        }
+        
+    }
+}
