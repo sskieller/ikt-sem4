b9b23263d684b70edbcde83fe71e5943c3eb3793
Simon Vu <Simon_vu1@hotmail.com>
2018-05-14 15:18:07 +0200

    Added DocumentController

---
+++ DAB/DABHandin4/SmartGrid/Controllers/TransactionController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using SmartGrid.Models;
+using SmartGrid.Repositories;
+
+namespace SmartGrid.Controllers
+{
+    public class TransactionController : ApiController
+    {
+        private const string _endpointUrl = "https://localhost:8081";
+        private const string _key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        private readonly DocumentClient _client;
+
+        private TransactionRepository _repository;
+
+
+        public TransactionController()
+        {
+            _client = new DocumentClient(new Uri(_endpointUrl), _key);
+            Setup().Wait(); //Wait for setup to complete
+
+            _repository = new TransactionRepository(_client, "TransactionDB", "TransactionCollection");
+        }
+
+        private async Task Setup()
+        {
+
+            _client.CreateDatabaseIfNotExistsAsync(new Database() { Id = "TransactionDB" }).Wait(2000);
+            _client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("TransactionDB"),
+                new DocumentCollection { Id = "PersonCollection" }).Wait(2000);
+
+        }
+        // GET: api/Person
+        public async Task<IEnumerable<Transaction>> Get()
+        {
+            return await _repository.ReadAll();
+        }
+
+        // GET: api/Person/5
+        public async Task<Transaction> Get(string id)
+        {
+            return await _repository.Read(id);
+        }
+
+        // POST: api/Person
+        public async Task Post([FromBody]Transaction p)
+        {
+            //Person p = await Task<Person>.Factory.StartNew(() => JsonConvert.DeserializeObject<Person>(value));
+            await _repository.Create(p);
+
+        }
+
+        // PUT: api/Person/5
+        public async Task Put(int id, [FromBody]Transaction p)
+        {
+            //Person p = await Task<Person>.Factory.StartNew(() => JsonConvert.DeserializeObject<Person>(value));
+
+            if (p == null)
+            {
+                return;
+            }
+            await _repository.Update(p.Id, p);
+
+        }
+
+        // DELETE: api/Person/5
+        public async Task Delete(string id)
+        {
+
+            if (id == null)
+            {
+                return;
+            }
+
+            await _repository.Delete(id);
+
+        }
+    }
+}
