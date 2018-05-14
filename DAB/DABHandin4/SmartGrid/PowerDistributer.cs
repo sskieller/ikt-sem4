559d89e3e394b11c2d36930014153d4a5ce127be
Simon Vu <Simon_vu1@hotmail.com>
2018-05-14 14:26:56 +0200

    WUT

---
+++ DAB/DABHandin4/SmartGrid/PowerDistributer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SmartGrid.Models;
+namespace SmartGrid
+{
+    public class PowerDistributer
+    {
+        public PowerDistributer(SmartGridContext a)
+        {
+
+        }
+    }
+}
