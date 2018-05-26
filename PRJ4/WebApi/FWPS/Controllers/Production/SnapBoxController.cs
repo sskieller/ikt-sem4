851630715e0d238d4635a62c2d4c407a789292f1
Simon Vu <Simon_vu1@hotmail.com>
2018-05-26 13:52:30 +0200

    Added WebAPI function to put test data in DB

--- PRJ4/WebApi/FWPS/Controllers/Production/SnapBoxController.cs
+++ PRJ4/WebApi/FWPS/Controllers/Production/SnapBoxController.cs
@@ -55,6 +55,48 @@ namespace FWPS.Controllers
 	        return items;
 	    }
 
+	    [HttpGet("[action]")] // '/api/snapbox/Testupdate'
+	    public IEnumerable<SnapBoxItem> TestUpdate()
+	    {
+	        if (_context.SnapBoxItems.ToList().Count <= 0)
+	        {
+	            throw new NoItemsInDatabaseException();
+	        }
+
+	        var items = from it in _context.SnapBoxItems
+	            where DateTime.Now.Subtract(it.CreatedDate) < TimeSpan.FromDays(7)
+                select it;
+
+	        foreach (var item in items)
+	        {
+	            _context.SnapBoxItems.Remove(item);
+	        }
+
+	        DateTime[] dates = new DateTime[20];
+            List<int> daysFromNow = new List<int>(20)
+            {
+                6, 6, 5, 5, 5, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,2, 1, 1
+            };
+            for (int i = 0; i < 20; ++i)
+            {
+                dates[i] = DateTime.Now - TimeSpan.FromDays(daysFromNow[i]);
+                _context.SnapBoxItems.Add(new SnapBoxItem()
+	            {
+                    Checksum = "TEST",
+                    CreatedDate = dates[i],
+                    LastModifiedDate = dates[i],
+                    MailReceived = false,
+                    PowerLevel = "100",
+                    ReceiverEmail = "",
+                    SnapBoxId = "0xTEST"
+                });
+	        }
+
+            _context.SaveChanges();
+
+            return items;
+	    }
+
         [HttpGet("{id:int}", Name = "GetSnapBox")]
 		public IActionResult GetById(long id)
 		{
